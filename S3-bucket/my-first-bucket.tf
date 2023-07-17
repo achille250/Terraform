17a89b9aac200f7d710db854283ea8bf6dbf65d2
achille250 <106239625+achille250@users.noreply.github.com>
2023-07-17 22:57:10 +0200

    S3 bucket for uploading a file

--- S3-bucket/my-first-bucket.tf
+++ S3-bucket/my-first-bucket.tf
@@ -1,47 +1,22 @@
-
-
 provider "aws" {
   region = "us-east-1"
- }
+  access_key = "AKIA4P6KYPSARB6BUEOK"
+  secret_key = "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"
+} 
 
-  # Create an S3 bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-first-buckt"  
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_buckets" {
+  bucket = "my-first-buckt-250"
 
   tags = {
     Name = "S3-bucket"
   }
 }
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.id
 
-  acl = "private"
+#Upload a file to the bucket
+resource "aws_s3_object" "file" {
+  bucket = aws_s3_bucket.my_buckets.id
+  key    = "terraform_command.txt"
+  source = "D:/Documents/GitHub/Terraform/S3-bucket/terraform_command.txt"
+
 }
-
-# Create an IAM user
-resource "aws_iam_user" "cesar_user" {
-  name = "cesar" 
-}
-
-# Create an IAM access key and secret for the user
-resource "aws_iam_access_key" "cesar_user_key" {
-  user = aws_iam_user.cesar_user.name
-}
-
-# Attach a policy to the IAM user granting access to the S3 bucket
-resource "aws_iam_user_policy_attachment" "cesar_user_policy" {
-  user       = aws_iam_user.cesar_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess" 
-}
-
-# Output the access key and secret for the user
-output "access_key" {
-  value = aws_iam_access_key.cesar_user_key.id
-  sensitive = true
-}
-
-output "secret_key" {
-  value = aws_iam_access_key.cesar_user_key.secret
-  sensitive = true
-}
-
